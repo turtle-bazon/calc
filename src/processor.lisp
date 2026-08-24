654cdf10b182d8a5c08233cd7551745cf46a5194
turtle <turtle@bazon.ru>
2026-08-24 22:22:19 +0300

    refactor: processor.lisp helpers reordered to match dispatch order

--- src/processor.lisp
+++ src/processor.lisp
@@ -18,48 +18,26 @@
       (error (c)
         (format t "Error: ~a~%" c)))))
 
-(defun handle-call-form (tr vars funcs)
-  (let ((func (gethash (string-upcase (first (uiop:split-string tr :separator '(#\Space #\Tab #\()))) funcs)))
-             (when func
-               (let* ((args-str (subseq tr (1+ (position #\( tr))))
-                      (args-str (subseq args-str 0 (position #\) args-str)))
-                      (arg-vals (mapcar #'eval-rpn
-                                        (uiop:split-string args-str :separator '(#\,))
-                                        (make-list (length (uiop:split-string args-str :separator '(#\,))) :initial-element vars)
-                                        (make-list (length (uiop:split-string args-str :separator '(#\,))) :initial-element funcs)))
-                      (arg-names (getf func :args))
-                      (body (getf func :body)))
-                 (dolist (pair (mapcar #'cons arg-names arg-vals))
-                   (setf (gethash (string-upcase (car pair)) vars) (cdr pair)))
-                 (let ((result (eval-rpn body vars funcs)))
-                   (format t "= ~a~%" result)
-                   (dolist (pn arg-names) (remhash (string-upcase pn) vars))
-                   t)))))
 
-(defun handle-defun-form (tr vars funcs)
-  (when (let ((pos (search "defun " tr :test #'string-equal)))
-             (and pos (= pos 0)))
-    (let* ((parts (uiop:split-string tr :separator '(#\Space #\Tab)))
-                  (raw-name (second parts))
-                  ;; Strip "(params)" from the name so lookups match calls
-                  (name (if raw-name
-                            (let ((p (position #\( raw-name)))
-                              (if p (subseq raw-name 0 p) raw-name))))
-                  (arglist-start (position #\( tr))
-                  (arglist-end (position #\) tr :start arglist-start))
-                  (body-start (1+ arglist-end))
-                  (args-str (subseq tr (1+ arglist-start) arglist-end))
-                  (parsed-args (when (> (length args-str) 0)
-                                 (mapcar #'string-trim
-                                         (make-list (length (uiop:split-string args-str :separator '(#\, #\Space)))
-                                                    :initial-element '(#\Space #\Tab))
-                                         (uiop:split-string args-str :separator '(#\, #\Space)))))
-                  (args parsed-args)
-                  (body (subseq tr body-start)))
-             (when (and name args)
-               (setf (gethash (string-upcase name) funcs)
-                     (list :args args :body body))
-               (format t "Defined function: ~a~%" name)))))
+
+
+(defun handle-assignment (tr vars funcs)
+  (when (let ((eq (position (char "=" 0) tr :from-end t))
+                 (par (position (char "(" 0) tr)))
+             (and eq
+                  (not par)
+                  (let ((lhs (string-trim '(#\Space #\Tab) (subseq tr 0 eq))))
+                    (and (> (length lhs) 0)
+                         (every #'alpha-char-p lhs)
+                         (> eq 0)
+                         (< (1+ eq) (length tr))))))
+    (let* ((eq (position (char "=" 0) tr))
+                  (vn (string-trim '(#\Space #\Tab) (subseq tr 0 eq)))
+                  (r (eval-rpn (subseq tr (1+ eq)) vars funcs)))
+             (when (and (> (length vn) 0) (every #'alpha-char-p vn))
+               (setf (gethash vn vars) r)
+               (format t "= ~a~%" r)))))
+
 
 (defun handle-defmacro-form (tr vars funcs)
   (when (let ((pos (search "defmacro " tr :test #'string-equal)))
@@ -87,21 +65,49 @@
                      (list :args (or args nil) :body body))
                (format t "Defined macro: ~a~%" name)))))
 
-(defun handle-assignment (tr vars funcs)
-  (when (let ((eq (position (char "=" 0) tr :from-end t))
-                 (par (position (char "(" 0) tr)))
-             (and eq
-                  (not par)
-                  (let ((lhs (string-trim '(#\Space #\Tab) (subseq tr 0 eq))))
-                    (and (> (length lhs) 0)
-                         (every #'alpha-char-p lhs)
-                         (> eq 0)
-                         (< (1+ eq) (length tr))))))
-    (let* ((eq (position (char "=" 0) tr))
-                  (vn (string-trim '(#\Space #\Tab) (subseq tr 0 eq)))
-                  (r (eval-rpn (subseq tr (1+ eq)) vars funcs)))
-             (when (and (> (length vn) 0) (every #'alpha-char-p vn))
-               (setf (gethash vn vars) r)
-               (format t "= ~a~%" r)))))
+
+(defun handle-defun-form (tr vars funcs)
+  (when (let ((pos (search "defun " tr :test #'string-equal)))
+             (and pos (= pos 0)))
+    (let* ((parts (uiop:split-string tr :separator '(#\Space #\Tab)))
+                  (raw-name (second parts))
+                  ;; Strip "(params)" from the name so lookups match calls
+                  (name (if raw-name
+                            (let ((p (position #\( raw-name)))
+                              (if p (subseq raw-name 0 p) raw-name))))
+                  (arglist-start (position #\( tr))
+                  (arglist-end (position #\) tr :start arglist-start))
+                  (body-start (1+ arglist-end))
+                  (args-str (subseq tr (1+ arglist-start) arglist-end))
+                  (parsed-args (when (> (length args-str) 0)
+                                 (mapcar #'string-trim
+                                         (make-list (length (uiop:split-string args-str :separator '(#\, #\Space)))
+                                                    :initial-element '(#\Space #\Tab))
+                                         (uiop:split-string args-str :separator '(#\, #\Space)))))
+                  (args parsed-args)
+                  (body (subseq tr body-start)))
+             (when (and name args)
+               (setf (gethash (string-upcase name) funcs)
+                     (list :args args :body body))
+               (format t "Defined function: ~a~%" name)))))
+
+
+(defun handle-call-form (tr vars funcs)
+  (let ((func (gethash (string-upcase (first (uiop:split-string tr :separator '(#\Space #\Tab #\()))) funcs)))
+             (when func
+               (let* ((args-str (subseq tr (1+ (position #\( tr))))
+                      (args-str (subseq args-str 0 (position #\) args-str)))
+                      (arg-vals (mapcar #'eval-rpn
+                                        (uiop:split-string args-str :separator '(#\,))
+                                        (make-list (length (uiop:split-string args-str :separator '(#\,))) :initial-element vars)
+                                        (make-list (length (uiop:split-string args-str :separator '(#\,))) :initial-element funcs)))
+                      (arg-names (getf func :args))
+                      (body (getf func :body)))
+                 (dolist (pair (mapcar #'cons arg-names arg-vals))
+                   (setf (gethash (string-upcase (car pair)) vars) (cdr pair)))
+                 (let ((result (eval-rpn body vars funcs)))
+                   (format t "= ~a~%" result)
+                   (dolist (pn arg-names) (remhash (string-upcase pn) vars))
+                   t)))))
 
 
